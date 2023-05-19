b1478571bc1b8d3beb2b10aa0bb7f9e86f946b58
drkenni <tanyromachka1@mail.ru>
2023-05-19 13:19:25 +0300

    Fix

--- src/item.py
+++ src/item.py
@@ -54,7 +54,7 @@ class Item:
         if len(new_name) <= 10:
             self.__name = new_name
         else:
-            print("Длина наименования товара превышает 10 символов")
+            raise TypeError("Длина наименования товара превышает 10 символов")
 
     @classmethod
     def instantiate_from_csv(cls):
